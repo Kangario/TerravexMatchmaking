f6832a994c6ae69485334f6508ce848f2c6557af
Alexander Trushkov <mr.trushkoff2016@mail.ru>
2026-01-22 11:08:26 +0300

    Init Server

--- Server.js
+++ Server.js
@@ -178,9 +178,12 @@ async function start() {
         }
     });
 
-    app.listen(3000, () => {
-        console.log("🚀 Matchmaking server running on http://localhost:3000");
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`🚀 Matchmaking server running on port ${PORT}`);
     });
+
 }
 
 start();
